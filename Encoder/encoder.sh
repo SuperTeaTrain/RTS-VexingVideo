7c48193069b84941eacc674c7e86228e4938c9c1
Jack D <31171502+JackTildeD@users.noreply.github.com>
2020-03-24 15:29:13 -0500

    Update encoder.sh

--- Encoder/encoder.sh
+++ Encoder/encoder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # --- 80 Columns ------------------------------------------------------------- #
 # Description:
 # Author: Jack~D
@@ -6,6 +6,7 @@
 DIRECTORY=$(cd $(dirname ${0}) && pwd) # Path to this directory
 INPUT='' # Path to input video file
 OUTPUT='' # Path to output directory
+FPS=''
 
 # Flags
 DEBUG=false # Debug mode
@@ -21,6 +22,7 @@ WHITE=$(tput sgr0)
 # Print help information
 helpText() {
    echo -e ' -d --debug\tDebug mode'
+   echo -e ' -f --fps\tOutput frames per second'
    echo -e ' -h --help\tHelp information'
    echo -e ' -i --input\tPath to input video file'
    echo -e ' -o --output\tPath to output directory'
@@ -35,23 +37,26 @@ strip() {
 
 handleParameters() {
    let 'COUNT = 0'
-   while [ "$1" != '' ]
+   while [ "${1}" != '' ]
    do
-      PARAM=$(echo $1 | awk -F= '{print $1}')
-      VALUE=$(echo $1 | awk -F= '{print $2}')
-      case $PARAM in
+      PARAM=$(echo ${1} | awk -F= '{print $1}')
+      VALUE=$(echo ${1} | awk -F= '{print $2}')
+      case "${PARAM}" in
          -d | --debug )
             DEBUG=true
             ;;
-         -h | --help | 'help' | 'Help' )
+         -f | --fps )
+            FPS=$(echo ${VALUE} | strip)
+            ;;
+         -h | --help | 'help' | 'Help' | 'HELP' )
             helpText # Print help information
             exit '0'
             ;;
          -i | --input )
-            INPUT=$(echo $VALUE | strip)
+            INPUT=$(echo ${VALUE} | strip)
             ;;
          -o | --output )
-            OUTPUT=$(echo $VALUE | strip)
+            OUTPUT=$(echo ${VALUE} | strip)
             ;;
          -q | --quiet )
             QUIET=true
@@ -119,6 +124,7 @@ handleParameters() {
    if [ ${DEBUG} = true -a ${QUIET} = false ]
    then
       echo -e "Input = \"${INPUT}\"\nOutput = \"${OUTPUT}\""
+      echo "FPS = \"${FPS}\""
    fi
 }
 
@@ -135,8 +141,8 @@ dependencies() {
    then
       [ ${QUIET} = false ] && echo -e "\a[ ${GREEN}OK${WHITE} ] python3"
    else
-      echo -e -n "\a[ ${RED}ERROR${WHITE} ] python3 not in PATH. Please install "
-      echo 'python3 and add to PATH.'
+      echo -e -n "\a[ ${RED}ERROR${WHITE} ] python3 not in PATH. Please install"
+      echo ' python3 and add to PATH.'
       exit '1'
    fi
 }
@@ -145,8 +151,25 @@ main()
 {
    handleParameters "${@}"
    dependencies "${@}"
+   PARAMS="'${INPUT}' '${OUTPUT}/%10d.png'"
+   [ "${QUIET}" = true ] && PARAMS=$(echo -n "${PARAMS} -loglevel 'fatal'")
+   [ "${FPS}" != '' ] && PARAMS=$(echo -n "${PARAMS} -vf fps='${FPS}'")
+   #if [ ${DEBUG} = true -a ${QUIET} = false ]
+   #then
+   echo "PARAMS = \"${PARAMS}\""
+   #fi
+   echo -n "xargs ffmpeg -i ${PARAMS}"  | sh || exit '1'
+   #if [ $(echo ${FPS} | strip) = '' ]
+   #then
+   #   ffmpeg -i "${INPUT}" "${OUTPUT}/$filename%10d.png" || exit '1'
+   #else
+   #   ffmpeg -i "${INPUT}" -vf fps="${FPS}" "${OUTPUT}/$filename%10d.png" \
+   #      -loglevel 'quiet' \
+   #      || exit '1'
+   #fi
 }
 
 main "${@}"
 
 # END OF LINE
+
