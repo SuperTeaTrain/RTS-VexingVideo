15bf35b0543f1f950277f6db19676b1ec16cf8f5
Jack Dixon III <JackTildeD@Protonmail.com>
2020-05-02 18:42:05 -0500

    Update

--- Main/Test.py
+++ Main/Test.py
@@ -3,79 +3,62 @@ from tkinter import *
 from PIL import ImageTk, Image
 import time
 
+# --- 80 Columns ------------------------------------------------------------- #
+
 UPDATE_RATE = int(1000/16)
 
-#root = Tk()  
-#canvas = Canvas(root, width = 300, height = 300)  
-#canvas.pack()  
-#img = ImageTk.PhotoImage(Image.open("Frames/ball.png"))  
-#canvas.create_image(20, 20, anchor=NW, image=img) 
-#root.mainloop()
-
-frames = [[filename[10], 'Frames/' + filename] for filename in os.listdir('Frames') if os.path.isfile(os.path.join('Frames', filename))]
-frames = sorted(frames, key=lambda x: x[1])
-
-#root = Tk()  
-#canvas = Canvas(root, width = 300, height = 300)  
-#canvas.pack()  
-#img = ImageTk.PhotoImage(Image.open(frames[0]))  
-#canvas.create_image(20, 20, anchor=NW, image=img) 
-#root.mainloop()
-
-def place(root, x, i):
-    label3 = Label(root, image=x)
-    label3.image = x
-    label3.place(x=50+i, y=50)
-    return
-
-class App(Frame):
+class App():
     def __init__(self):
         self.t = 0
         self.root = Tk()
         self.root.title('Vexing Video')
-        self.imgs = []
-        self.array = []
-        self.on_init()
+        return
+    def start(self):
         self.on_loop()
         self.root.mainloop()
         return
-    def on_init(self):
-        self.imgs = [[i[0], ImageTk.PhotoImage(Image.open(i[1]))] for i in frames]
-        #for i in range(len(frames)):
-        #    self.imgs += [ImageTk.PhotoImage(Image.open(frames[i]))]
-        self.canvas1 = Canvas(width=654, height=480)
-        self.canvas1.pack()
-        self.width1 = self.imgs[self.t][1].width()
-        self.height1 = self.imgs[self.t][1].height()
-        #for r in range(3):
-        #    for c in range(3):
-        #        if r == 1 and c == 1:
-        #            # 654x480
-        #            #self.img = ImageTk.PhotoImage(Image.open(frames[self.t]))
-        #            #Label(self.root, image=self.img,
-        #            #    borderwidth=1).grid(row=r,column=c)
-        #        else:
-        #            Label(self.root, text='X',
-        #                borderwidth=1).grid(row=r,column=c)
-        return
     def on_cleanup(self):
         return
+    def create_canvas(self, width, height):
+        self.width1 = width
+        self.height1 = height
+        self.canvas1 = Canvas(width=self.width1, height=self.height1)
+        self.canvas1.pack()
+        return
+    def set_frames(self, frames):
+        self.frames = frames
+        return
     def on_loop(self):
-        #self.img = ImageTk.PhotoImage(Image.open(self[self.t][1]))
-        #Label(self.root, image=self.imgs[self.t][1],
-        #    borderwidth=1).grid(row=1,column=1)
-        if self.t % len(frames) == 0:
-            self.canvas1.delete("all")
-        self.canvas1.create_image(self.width1/2, self.height1/2, image=self.imgs[self.t][1])
-        self.t = (self.t + 1) % len(frames)
+        if self.t % len(self.frames) == 0:
+            self.canvas1.delete('all')
+        self.canvas1.create_image(
+            self.width1/2,
+            self.height1/2,
+            image=self.frames[self.t][1]
+        )
+        self.t = (self.t + 1) % len(self.frames)
         self.root.after(UPDATE_RATE, self.on_loop)
         return
 
 def main():
     app = App()
+    frames = [
+        [filename[10], 'Frames/' + filename]
+        for filename
+        in os.listdir('Frames')
+        if os.path.isfile(os.path.join('Frames', filename))
+    ]
+    frames = sorted(frames, key=lambda x: x[1])
+    frames = [
+        [i[0], ImageTk.PhotoImage(Image.open(i[1]))]
+        for i
+        in frames
+    ]
+    app.create_canvas(frames[0][1].width(), frames[0][1].height())
+    app.set_frames(frames)
+    app.start()
     return
 
 if __name__ == "__main__" :
     main()
-    #print(str(frames))
 
